f5635c4a715342cf7bc8aa4e1cb88c25f8c3d491
weitzgm <weitzgm@gmail.com>
2023-12-05 20:24:33 -0700

    reverse, u32 gives different answer? not panic?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 mod puzzle_1;
 mod puzzle_2;
 mod puzzle_3;
@@ -5,5 +7,8 @@ mod puzzle_4;
 mod puzzle_5;
 
 fn main() {
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("{}", puzzle_5::solve());
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    println!("Executed in {} seconds", (end-start).as_secs());
 }
--- src/puzzle_5.rs
+++ src/puzzle_5.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::{collections::HashMap, fs::read_to_string};
+use std::{collections::HashMap, fs::read_to_string, time::{SystemTime, UNIX_EPOCH}};
 
 pub fn solve() -> u64 {
     let filepath = "assets/puzzle5/input.txt";
@@ -10,7 +10,7 @@ pub fn solve() -> u64 {
     let mut current_map = "";
 
     let seed_regex = regex::Regex::new(r"(\d+ \d+)").unwrap();
-    let mut seeds = Vec::<u64>::new();
+    let mut seeds = Vec::<(u64, u64)>::new();
 
     let map_regex = regex::Regex::new(r"(?<name>\w+)-to").unwrap();
     let mut maps = HashMap::<&str, Vec<(u64, u64, u64)>>::new();
@@ -21,8 +21,7 @@ pub fn solve() -> u64 {
                 let pair_split: Vec<&str> = m.get(0).unwrap().as_str().split(" ").collect();
                 let start = pair_split[0].parse::<u64>().unwrap();
                 let length = pair_split[1].parse::<u64>().unwrap();
-                let mut range: Vec<u64> = (start..start+length).collect();
-                seeds.append(&mut range);
+                seeds.push((start, length));
                 // seeds.push(m.get(0).unwrap().as_str().parse::<u64>().unwrap());
             }
         } else if line.contains("map") {
@@ -36,16 +35,27 @@ pub fn solve() -> u64 {
             maps.get_mut(current_map).unwrap().push((tuple_vec[0], tuple_vec[1], tuple_vec[2]));
         }
     }
-    let mut smallest = u64::MAX;
-    for seed in seeds {
-        //println!("Starting at {}", seed);
-        let res = trace_seed(seed, &maps);
-        //println!("  ended at {}", res);
-        if res < smallest {
-            smallest = res;
+    // let mut smallest = u64::MAX;
+    // for seed in seeds {
+    //     //println!("Starting at {}", seed);
+    //     // let res = trace_seed(seed, &maps);
+    //     //println!("  ended at {}", res);
+    //     if res < smallest {
+    //         smallest = res;
+    //     }
+    // }
+    // smallest
+    let mut counter: u64 = 0;
+    'outer: loop {
+        let seed = trace_loc(counter, &maps);
+        //println!("trying {}", counter);
+        for s in &seeds {
+            if s.0 <= seed && seed < s.0 + s.1{
+                break 'outer counter;
+            }
         }
+        counter += 1
     }
-    smallest
 }
 
 fn trace_seed(seed: u64, maps: &HashMap<&str, Vec<(u64, u64, u64)>>) -> u64{
@@ -59,6 +69,29 @@ fn trace_seed(seed: u64, maps: &HashMap<&str, Vec<(u64, u64, u64)>>) -> u64{
                     if values.1 <= search_key && search_key < values.1 + values.2 {
                         //println!(" within range {:?}", values);
                         search_key = values.0 + (search_key - values.1);
+                        break;
+                    }
+                }
+            }
+            None => break
+        }
+        //println!(" to {}", search_key);
+    }
+    search_key
+}
+
+fn trace_loc(loc: u64, maps: &HashMap<&str, Vec<(u64, u64, u64)>>) -> u64{
+    let mut transitions = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity"];
+    transitions.reverse();
+    let mut search_key = loc;
+    for trans in transitions {
+        //println!(" through {}", trans);
+        match maps.get(trans) {
+            Some(ranges) => {
+                for values in ranges {
+                    if values.0 <= search_key && search_key < values.0 + values.2 {
+                        //println!(" within range {:?}", values);
+                        search_key = values.1 + (search_key - values.0);
                         break;
                     }
                 }
