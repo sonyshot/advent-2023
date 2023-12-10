d462cd004f5a12b87953702c32cf0604d5a7f371
sonyshot <weitzgm@gmail.com>
2023-12-10 08:34:09 -0700

    jeez

--- src/puzzle_10.rs
+++ src/puzzle_10.rs
@@ -1,22 +1,111 @@
 use regex::Regex;
-use std::{collections::HashMap, fs::read_to_string};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::read_to_string,
+};
 
 pub fn solve() -> u64 {
     let file_path = "assets/puzzle10/input.txt";
     let file_lines = read_to_string(file_path).unwrap();
     let file_lines_lines: Vec<&str> = file_lines.lines().collect();
-    one(file_lines_lines)
+    two(file_lines_lines)
+}
+
+fn two(lines: Vec<&str>) -> u64 {
+    let width = lines[0].len();
+    let height = lines.len();
+    let (map, start) = create_map(lines);
+
+    let pipe_path = find_path(&map, start);
+
+    let mut area = 0;
+
+    (0..height).into_iter().for_each(|row| {
+        let mut inside = false;
+        let mut path_leg: Option<Tile> = None;
+        (0..width).into_iter().for_each(|col| {
+            if pipe_path.contains(&(row, col)) {
+                match map[row][col] {
+                    Tile::UpDown => {
+                        inside = !inside;
+                    }
+                    Tile::DownLeft => {
+                        if let Some(pl) = path_leg {
+                            if pl == Tile::UpRight {
+                                inside = !inside;
+                            } else {
+                                path_leg = None;
+                            }
+                        }
+                    }
+                    Tile::UpLeft => {
+                        if let Some(pl) = path_leg {
+                            if pl == Tile::DownRight {
+                                inside = !inside;
+                            } else {
+                                path_leg = None;
+                            }
+                        }
+                    }
+                    Tile::UpRight => path_leg = Some(Tile::UpRight),
+                    Tile::DownRight => path_leg = Some(Tile::DownRight),
+                    Tile::Start => {
+                        // by inspection, this puzzle has Start treated as UpLeft
+                        if let Some(pl) = path_leg {
+                            if pl == Tile::DownRight {
+                                inside = !inside;
+                            } else {
+                                path_leg = None;
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            } else if inside {
+                area += 1;
+            }
+        })
+    });
+
+    area
 }
 
 fn one(lines: Vec<&str>) -> u64 {
-    let mut sum = 0;
+    let (map, start) = create_map(lines);
 
+    let pipe_path = find_path(&map, start);
+
+    pipe_path.len() as u64 / 2
+}
+
+fn create_map(lines: Vec<&str>) -> (Vec<Vec<Tile>>, (usize, usize)) {
     let width = lines[0].len();
     let height = lines.len();
 
     let mut map = vec![vec![Tile::None; width]; height];
     let mut start_point = (0, 0);
 
+    for (i, line) in lines.iter().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            match c {
+                '│' => map[i][j] = Tile::UpDown,
+                '┘' => map[i][j] = Tile::UpLeft,
+                '┌' => map[i][j] = Tile::DownRight,
+                '┐' => map[i][j] = Tile::DownLeft,
+                '└' => map[i][j] = Tile::UpRight,
+                '─' => map[i][j] = Tile::LeftRight,
+                'S' => {
+                    map[i][j] = Tile::Start;
+                    start_point = (i, j)
+                }
+                _ => map[i][j] = Tile::None,
+            }
+        }
+    }
+    return (map, start_point);
+}
+
+fn find_path(map: &Vec<Vec<Tile>>, start_point: (usize, usize)) -> HashSet<(usize, usize)> {
     // key is direction of motion onto tile X and value is corresponding next step
     let directionality = HashMap::<(Direction, Tile), (i32, i32, Direction)>::from([
         ((Direction::Left, Tile::DownRight), (1, 0, Direction::Down)),
@@ -36,23 +125,8 @@ fn one(lines: Vec<&str>) -> u64 {
         ((Direction::Down, Tile::UpDown), (1, 0, Direction::Down)),
     ]);
 
-    for (i, line) in lines.iter().enumerate() {
-        for (j, c) in line.chars().enumerate() {
-            match c {
-                '│' => map[i][j] = Tile::UpDown,
-                '┘' => map[i][j] = Tile::UpLeft,
-                '┌' => map[i][j] = Tile::DownRight,
-                '┐' => map[i][j] = Tile::DownLeft,
-                '└' => map[i][j] = Tile::UpRight,
-                '─' => map[i][j] = Tile::LeftRight,
-                'S' => {
-                    map[i][j] = Tile::Start;
-                    start_point = (i, j)
-                }
-                _ => map[i][j] = Tile::None,
-            }
-        }
-    }
+    let mut output = HashSet::<(usize, usize)>::new();
+    output.insert(start_point);
 
     let mut curr_index: (usize, usize) = start_point;
     let mut curr_dir: Direction = Direction::Up;
@@ -66,18 +140,17 @@ fn one(lines: Vec<&str>) -> u64 {
 
     for test in from_start {
         if let Some(thing) = directionality.get(&test) {
-            sum += 1;
             curr_index = (
                 (curr_index.0 as i32 + thing.0) as usize,
                 (curr_index.1 as i32 + thing.1) as usize,
             ); //weeee casting
+            output.insert(curr_index);
             curr_dir = thing.2;
             break;
         }
     }
 
     loop {
-        sum += 1;
         if map[curr_index.0][curr_index.1] == Tile::Start {
             break;
         }
@@ -86,6 +159,7 @@ fn one(lines: Vec<&str>) -> u64 {
                 (curr_index.0 as i32 + thing.0) as usize,
                 (curr_index.1 as i32 + thing.1) as usize,
             ); //weeee casting
+            output.insert(curr_index);
             curr_dir = thing.2;
         } else {
             println!("Woopsie");
@@ -96,8 +170,7 @@ fn one(lines: Vec<&str>) -> u64 {
             break;
         }
     }
-
-    sum / 2
+    output
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
